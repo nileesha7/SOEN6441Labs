e6e76593a9e6f150559159106be9e9eea6b00d1f
Nileesha Fernando <nileeshasajani@hotmail.com>
2018-07-12 14:58:34 -0400

    Initial commit

---
+++ src/com/SOEN6441/Lab/Tests/FilteringApplesTest.java
@@ -0,0 +1,20 @@
+package com.SOEN6441.Lab.Tests;
+
+import com.SOEN6441.Lab.Main.FilteringApples.Apple;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import org.junit.jupiter.api.Test;
+
+class FilteringApplesTest {
+
+	@Test
+	void test() {
+	}
+
+}
