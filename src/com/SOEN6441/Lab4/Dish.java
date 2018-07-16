e03d44cdb3db068a9377d4628d54bc12ed91ee41
Nileesha Fernando <nileeshasajani@hotmail.com>
2018-07-16 12:13:58 -0400

    lab 4 upto task 2

---
+++ src/com/SOEN6441/Lab4/Dish.java
@@ -0,0 +1,62 @@
+package com.SOEN6441.Lab4;
+import java.util.*;
+import static java.util.stream.Collectors.toList;
+
+public class Dish {
+
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type { MEAT, FISH, OTHER }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static final List<Dish> menu =
+            Arrays.asList( new Dish("pork", false, 800, Dish.Type.MEAT),
+                           new Dish("beef", false, 700, Dish.Type.MEAT),
+                           new Dish("chicken", false, 400, Dish.Type.MEAT),
+                           new Dish("french fries", true, 530, Dish.Type.OTHER),
+                           new Dish("rice", true, 350, Dish.Type.OTHER),
+                           new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                           new Dish("pizza", true, 550, Dish.Type.OTHER),
+                           new Dish("prawns", false, 400, Dish.Type.FISH),
+                           new Dish("salmon", false, 450, Dish.Type.FISH));
+    
+    public static void main(String [] args) {
+    	//List<Dish> firstTwoMeatDishes = menu.stream().filter(dish -> "MEAT".equals(dish.getType().toString())).limit(2).collect(toList());
+    	List<Dish> firstTwoMeatDishes = menu.stream()
+    									.filter(dish -> "MEAT".equals(dish.getType().toString()))
+    									.limit(2)
+    									.collect(toList());
+
+    	System.out.println(firstTwoMeatDishes);
+    }
+}
---
+++ src/com/SOEN6441/Lab4/Mapping.java
@@ -0,0 +1,46 @@
+package com.SOEN6441.Lab4;
+
+import java.util.Arrays;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+public class Mapping {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1) Squares");
+
+		//Given a list of numbers, return a list of the square of each number
+		List <Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+		List <Integer> squares = numbers.stream()
+								.map(number->number*number)
+								.collect(toList());
+		System.out.println(squares);
+
+		System.out.println("-----");
+
+
+		//Given two lists of numbers, return all pairs of numbers
+		System.out.println("1) Pairs");
+		List <Integer> list1 = Arrays.asList(1,2,3);
+		List <Integer> list2 = Arrays.asList(3,4);
+
+		List<Integer[]> pairs = list1.stream()
+							  .flatMap(i->list2.stream().map(j->new Integer[] {i,j}))
+							  .collect(toList());
+		pairs.forEach(pair-> System.out.println("["+pair[0]+", "+pair[1]+"]"));
+
+		System.out.println("-----");
+		
+		//Extend the previous example to return only pairs whose sum is divisible by 3: For example, (2, 4) and (3, 3) are valid.
+		System.out.println("1) Pairs whose sum is divisible by 3");
+		List <Integer[]> pairsDivisibleBy3 = list1.stream()
+					.flatMap(i->list2.stream()
+							.map(j->new Integer[] {i,j})
+							.filter(pair -> (pair[0]+pair[1])%3 == 0))
+					.collect(toList());
+		
+		pairsDivisibleBy3.forEach(pair-> System.out.println("["+pair[0]+", "+pair[1]+"]"));
+	}
+
+}
